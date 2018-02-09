be07513a38b5a397727026882b3459a1b9a82818
Adam Scarr <adam@vektah.net>
2018-02-10 00:43:11 +1100

    add circle config

--- example/intkeys/user.go
+++ example/intkeys/user.go
@@ -3,19 +3,22 @@
 package intkeys
 
 import (
+	"strconv"
 	"time"
+
+	"github.com/vektah/dataloaden/example"
 )
 
 func NewLoader() *UserLoader {
 	return &UserLoader{
 		wait:     2 * time.Millisecond,
 		maxBatch: 100,
-		fetch: func(keys []string) ([]*User, []error) {
-			users := make([]*User, len(keys))
+		fetch: func(keys []int) ([]*example.User, []error) {
+			users := make([]*example.User, len(keys))
 			errors := make([]error, len(keys))
 
 			for i, key := range keys {
-				users[i] = &User{ID: key, Name: "user " + key}
+				users[i] = &example.User{ID: strconv.Itoa(key), Name: "user " + strconv.Itoa(key)}
 			}
 			return users, errors
 		},
